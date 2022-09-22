2ffc82f2c6c0ed41b1f320736eb26f2f47c4ca37
Roberto Cella <rob.uniud@gmail.com>
2022-09-22 22:33:36 +0200

    improved bot, now it manages to use the best image to convert

--- src/main/java/com/cellar/stickerifyimagebot/bot/StickerifyImageBot.java
+++ src/main/java/com/cellar/stickerifyimagebot/bot/StickerifyImageBot.java
@@ -1,18 +1,23 @@
 package com.cellar.stickerifyimagebot.bot;
 
+import com.cellar.stickerifyimagebot.image.ImageHelper;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
 import java.util.logging.Logger;
+
+import static java.util.Comparator.comparing;
 
 public class StickerifyImageBot extends TelegramLongPollingBot {
 
 	private static final Logger LOGGER = Logger.getLogger(StickerifyImageBot.class.getSimpleName());
 
-	private static final String SINGULAR_RESPONSE = "Your sticker file is ready!";
-	private static final String PLURAL_RESPONSE = "Your sticker files are ready!";
+	private static final String CAPTION = "Your sticker file is ready!";
 
 	@Override
 	public String getBotUsername() {
@@ -26,17 +31,18 @@ public class StickerifyImageBot extends TelegramLongPollingBot {
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		var photos = update.getMessage().getPhoto();
-		//List<File> pngFiles = photos.forEach(ImageHelper::convertToPng);
+		var images = update.getMessage().getPhoto();
+		var bestImage = images.stream().max(comparing(PhotoSize::getFileSize)).orElseThrow();
 
-		String chatId = update.getMessage().getChatId().toString();
-		SendMessage sendMessage = new SendMessage();
-		sendMessage.setChatId(chatId);
-		//sendMessage.setEntities(pngFiles);
-		sendMessage.setText(photos.size() > 1 ? PLURAL_RESPONSE : SINGULAR_RESPONSE);
+		SendDocument response = new SendDocument();
+		response.setChatId(update.getMessage().getChatId());
+		response.setCaption(CAPTION);
 
 		try {
-			execute(sendMessage);
+			File pngFile = ImageHelper.convertToPng(downloadFile(bestImage.getFilePath()));
+			response.setDocument(new InputFile(pngFile));
+
+			execute(response);
 		} catch (TelegramApiException e) {
 			LOGGER.severe("Unable to send the message: " + e);
 		}
