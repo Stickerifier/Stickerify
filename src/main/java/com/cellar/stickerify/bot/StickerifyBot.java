0b64aa92829ca7ebe34cc18c8e9adb93115d7391
Roberto Cella <rob.uniud@gmail.com>
2022-09-24 15:37:50 +0200

    renamed project in bot's messages

--- src/main/java/com/cellar/stickerify/bot/StickerifyBot.java
+++ src/main/java/com/cellar/stickerify/bot/StickerifyBot.java
@@ -19,13 +19,13 @@ public class StickerifyBot extends TelegramLongPollingBot {
 
 	private static final String FILE_READY_CAPTION = "Your sticker file is ready!";
 	private static final String ABOUT_TEXT = """
-			This bot is open source, you can check it out on [Github](https://github.com/rob93c/StickerifyImageBot)\\.
+			This bot is open source, you can check it out on [Github](https://github.com/rob93c/Stickerify)\\.
 			
 			Looking for sticker packs? Try [MeminiCustom](https://t.me/addstickers/MeminiCustom) and [VideoMemini](https://t.me/addstickers/VideoMemini)\\!
 			""";
 	private static final String ERROR_TEXT = """
             The file conversion was unsuccessful, only __valid image formats__ are supported \\(also `gif` and `webp` are not supported\\)\\.
-            If you think it should have worked, please report the issue on [Github](https://github.com/rob93c/StickerifyImageBot/issues/new/choose)\\.
+            If you think it should have worked, please report the issue on [Github](https://github.com/rob93c/Stickerify/issues/new/choose)\\.
             """;
 
 	@Override
