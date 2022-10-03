496c55b51fc9712042861fd14043e644d41181f7
Ankam Satya Sri Kanth <satyasrikanth.ankam@thoughtworks.com>
2022-10-03 21:04:57 +0530

    updated  markdown format in answer model to markdownv2 format

--- src/main/java/com/cellar/stickerify/telegram/Answer.java
+++ src/main/java/com/cellar/stickerify/telegram/Answer.java
@@ -6,26 +6,26 @@ package com.cellar.stickerify.telegram;
 public enum Answer {
 
 	HELP("""
-			Send me the image you want to convert and I will take care of the rest.
+			Send me the image you want to convert and I will take care of the rest\\.
 
 			Based on what you send, I will answer the following:
-			- the converted image, if you sent a supported file (currently `.gif`, `.gifv` and `.webp` files are not supported)
-			- an error message, if you sent an unsupported file
-			- an informative message for any message without a file
+			\\- the converted image, if you sent a supported file \\(currently `\\.gif`, `\\.gifv` and `\\.webp` files are not supported\\)
+			\\- an error message, if you sent an unsupported file
+			\\- an informative message for any message without a file
 			"""),
 	FILE_READY("""
-            Your sticker file is ready!
-            Head to [Stickers](https://t.me/Stickers) to create a new sticker.
+            Your sticker file is ready\\!
+            Head to [Stickers](https://t.me/Stickers) to create a new sticker\\.
             """),
 	ABOUT("""
-			This bot is open source, check it out on [Github](https://github.com/rob93c/Stickerify).
+			This bot is open source, check it out on [Github](https://github.com/rob93c/Stickerify)\\.
 
-			Looking for sticker packs? Try [MeminiCustom](https://t.me/addstickers/MeminiCustom) and [VideoMemini](https://t.me/addstickers/VideoMemini)!
+			Looking for sticker packs? Try [MeminiCustom](https://t.me/addstickers/MeminiCustom) and [VideoMemini](https://t.me/addstickers/VideoMeminis)\\!
 			"""),
 	ERROR("""
-			The file conversion was unsuccessful, only _valid image formats_ are supported (currently `.gif`, `.gifv` and `.webp` files are not supported).
+			The file conversion was unsuccessful, only _valid image formats_ are supported \\(currently `\\.gif`, `\\.gifv` and `\\.webp` files are not supported\\)\\.
 
-			If you think it should have worked, please report the issue on [Github](https://github.com/rob93c/Stickerify/issues/new/choose).
+			If you think it should have worked, please report the issue on [Github](https://github.com/rob93c/Stickerify/issues/new/choose)\\.
 			""", true);
 
 	private final String text;
