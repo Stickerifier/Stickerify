9ba9d4a7611eca9b6163a6698d72c69930e1f76d
Roberto Cella <rob.uniud@gmail.com>
2022-10-31 21:24:30 +0100

    added logger to main class

--- src/main/java/com/cellar/stickerify/runner/Main.java
+++ src/main/java/com/cellar/stickerify/runner/Main.java
@@ -1,18 +1,22 @@
 package com.cellar.stickerify.runner;
 
 import com.cellar.stickerify.bot.Stickerify;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
 		try {
 			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 			botsApi.registerBot(new Stickerify());
 		} catch (TelegramApiException e) {
-			e.printStackTrace();
+			LOGGER.error("An error occurred running the bot", e);
 		}
 	}
 }
