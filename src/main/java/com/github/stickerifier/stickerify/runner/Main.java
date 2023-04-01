95237a77e2cc3733b2b98e5e7270b6cb08cdd0fb
Roberto Cella <rob.uniud@gmail.com>
2023-04-01 13:38:36 +0200

    revert log changes in Main class

--- src/main/java/com/github/stickerifier/stickerify/runner/Main.java
+++ src/main/java/com/github/stickerifier/stickerify/runner/Main.java
@@ -1,22 +1,18 @@
 package com.github.stickerifier.stickerify.runner;
 
 import com.github.stickerifier.stickerify.bot.Stickerify;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
 	public static void main(String[] args) {
 		try {
 			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 			botsApi.registerBot(new Stickerify());
 		} catch (TelegramApiException e) {
-			LOGGER.warn("An unexpected error occurred: {}", e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
