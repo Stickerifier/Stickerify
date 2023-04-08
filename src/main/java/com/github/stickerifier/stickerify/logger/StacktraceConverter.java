645de48f1e2bdf53d2bf9a2c297344718ea5c2c1
Roberto Cella <rob.uniud@gmail.com>
2023-04-08 14:06:33 +0200

    added custom converter class to clear stacktraces from telegrambots

---
+++ src/main/java/com/github/stickerifier/stickerify/logger/StacktraceConverter.java
@@ -0,0 +1,25 @@
+package com.github.stickerifier.stickerify.logger;
+
+import ch.qos.logback.classic.pattern.ThrowableProxyConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.CoreConstants;
+
+/**
+ * Custom converter class to be used by Logback in order to clear the stacktrace
+ * of an exception raised in the package {@code org.telegram.telegrambots}.
+ */
+public class StacktraceConverter extends ThrowableProxyConverter {
+
+	private static final String TELEGRAMBOTS_ROOT_PACKAGE = "org.telegram.telegrambots";
+
+	@Override
+	public String convert(ILoggingEvent event) {
+		var throwable = event.getThrowableProxy();
+
+		if (throwable == null || throwable.getClassName() != null && event.getLoggerName().startsWith(TELEGRAMBOTS_ROOT_PACKAGE)) {
+			return CoreConstants.EMPTY_STRING;
+		}
+
+		return super.convert(event);
+	}
+}
