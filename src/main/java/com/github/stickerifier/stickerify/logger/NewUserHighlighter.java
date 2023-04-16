8fa492a3a974e83c2baecdf2491c4d878aee3b37
Roberto Cella <rob.uniud@gmail.com>
2023-04-16 21:49:56 +0200

    fixed issue with regex, changed highlight color to standard green

--- src/main/java/com/github/stickerifier/stickerify/logger/NewUserHighlighter.java
+++ src/main/java/com/github/stickerifier/stickerify/logger/NewUserHighlighter.java
@@ -1,13 +1,17 @@
 package com.github.stickerifier.stickerify.logger;
 
+import static ch.qos.logback.core.pattern.color.ANSIConstants.BOLD;
 import static ch.qos.logback.core.pattern.color.ANSIConstants.DEFAULT_FG;
 import static ch.qos.logback.core.pattern.color.ANSIConstants.ESC_END;
 import static ch.qos.logback.core.pattern.color.ANSIConstants.ESC_START;
+import static ch.qos.logback.core.pattern.color.ANSIConstants.GREEN_FG;
 import static com.github.stickerifier.stickerify.telegram.model.TelegramRequest.NEW_USER;
 
 import ch.qos.logback.classic.pattern.MessageConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.pattern.Converter;
+
+import java.util.regex.Pattern;
 
 /**
  * Custom converter class to be used by Logback in order to
@@ -17,8 +21,7 @@ import ch.qos.logback.core.pattern.Converter;
  */
 public class NewUserHighlighter extends MessageConverter {
 
-	private static final String BRIGHT_GREEN_FG = "92";
-	static final String START_GREEN = changeColorTo(BRIGHT_GREEN_FG);
+	static final String START_GREEN = changeColorTo(BOLD + GREEN_FG);
 	static final String RESET_COLOR = changeColorTo(DEFAULT_FG);
 	static final String HIGHLIGHTED_NEW_USER = START_GREEN + NEW_USER + RESET_COLOR;
 
@@ -27,7 +30,7 @@ public class NewUserHighlighter extends MessageConverter {
 		String message = event.getFormattedMessage();
 
 		if (message != null && message.contains(NEW_USER)) {
-			return message.replaceFirst(NEW_USER, HIGHLIGHTED_NEW_USER);
+			return message.replaceFirst(Pattern.quote(NEW_USER), HIGHLIGHTED_NEW_USER);
 		}
 
 		return message;
