df020f06f91baf69567158fe01bb7376cfbe779a
Roberto Cella <rob.uniud@gmail.com>
2023-05-01 18:46:23 +0200

    added logic to highlight mime types in logged messages, renamed converter class

---
+++ src/main/java/com/github/stickerifier/stickerify/logger/SubstringHighlighter.java
@@ -0,0 +1,59 @@
+package com.github.stickerifier.stickerify.logger;
+
+import static ch.qos.logback.core.pattern.color.ANSIConstants.BOLD;
+import static ch.qos.logback.core.pattern.color.ANSIConstants.DEFAULT_FG;
+import static ch.qos.logback.core.pattern.color.ANSIConstants.ESC_END;
+import static ch.qos.logback.core.pattern.color.ANSIConstants.ESC_START;
+import static ch.qos.logback.core.pattern.color.ANSIConstants.GREEN_FG;
+import static ch.qos.logback.core.pattern.color.ANSIConstants.YELLOW_FG;
+import static com.github.stickerifier.stickerify.telegram.model.TelegramRequest.NEW_USER;
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.Converter;
+
+import java.util.regex.Pattern;
+
+/**
+ * Custom converter class to be used by Logback in order to highlight important message substrings.
+ *
+ * @see Converter
+ */
+public class SubstringHighlighter extends MessageConverter {
+
+	private static final String START_YELLOW = changeColorTo(BOLD + YELLOW_FG);
+	static final String START_GREEN = changeColorTo(BOLD + GREEN_FG);
+	private static final String RESET_TEXT_ATTRIBUTES = "0;";
+	static final String RESET_COLOR = changeColorTo(RESET_TEXT_ATTRIBUTES + DEFAULT_FG);
+	static final String HIGHLIGHTED_NEW_USER = " " + START_YELLOW + NEW_USER.substring(1) + RESET_COLOR;
+	private static final String MIME_TYPE_REGEX = "[\\w-]+/[\\w-]+";
+
+	@Override
+	public String convert(ILoggingEvent event) {
+		String message = event.getFormattedMessage();
+
+		if (message != null) {
+			if (message.contains(NEW_USER)) {
+				return message.replaceFirst(Pattern.quote(NEW_USER), HIGHLIGHTED_NEW_USER);
+			}
+
+			var mimeType = getMimeType(message);
+			if (mimeType != null) {
+				var highlightedMimeType = START_GREEN + mimeType + RESET_COLOR;
+				return message.replaceFirst(mimeType, highlightedMimeType);
+			}
+		}
+
+		return message;
+	}
+
+	static String changeColorTo(final String color) {
+		return ESC_START + color + ESC_END;
+	}
+
+	private static String getMimeType(final String message) {
+		var matcher = Pattern.compile(MIME_TYPE_REGEX).matcher(message);
+
+		return matcher.find() ? matcher.group() : null;
+	}
+}
---
+++ src/test/java/com/github/stickerifier/stickerify/logger/SubstringHighlighterTest.java
@@ -0,0 +1,79 @@
+package com.github.stickerifier.stickerify.logger;
+
+import static com.github.stickerifier.stickerify.logger.SubstringHighlighter.HIGHLIGHTED_NEW_USER;
+import static com.github.stickerifier.stickerify.logger.SubstringHighlighter.RESET_COLOR;
+import static com.github.stickerifier.stickerify.logger.SubstringHighlighter.START_GREEN;
+import static com.github.stickerifier.stickerify.telegram.model.TelegramRequest.NEW_USER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SubstringHighlighterTest {
+
+	private static final String LOG_MESSAGE = "Received request";
+	private static final String MIME_TYPE = "image/png";
+	private static final String LOG_MESSAGE_WITH_MIME_TYPE = LOG_MESSAGE + " with " + MIME_TYPE + " MIME type";
+
+	private SubstringHighlighter substringHighlighter;
+
+	@BeforeEach
+	void setup() {
+		substringHighlighter = new SubstringHighlighter();
+	}
+
+	@Test
+	@DisplayName("Log message from old user")
+	void processEventWithOldUser() {
+		var event = new LoggingEvent(LOG_MESSAGE);
+
+		var convertedMessage = substringHighlighter.convert(event);
+
+		assertThat(convertedMessage, is(equalTo(LOG_MESSAGE)));
+	}
+
+	@Test
+	@DisplayName("Log message from new user")
+	void processEventWithNewUser() {
+		var event = new LoggingEvent(LOG_MESSAGE + NEW_USER);
+
+		var convertedMessage = substringHighlighter.convert(event);
+
+		assertThat(convertedMessage, is(equalTo(LOG_MESSAGE + HIGHLIGHTED_NEW_USER)));
+	}
+
+	@Test
+	@DisplayName("Log message with multiple new user occurrences")
+	void processEventWithMultipleNewUserOccurrences() {
+		var event = new LoggingEvent(LOG_MESSAGE + NEW_USER + NEW_USER);
+
+		var convertedMessage = substringHighlighter.convert(event);
+
+		assertThat(convertedMessage, is(equalTo(LOG_MESSAGE + HIGHLIGHTED_NEW_USER + NEW_USER)));
+	}
+
+	@Test
+	@DisplayName("Log message with MIME type")
+	void processEventWithMimeType() {
+		var event = new LoggingEvent(LOG_MESSAGE_WITH_MIME_TYPE);
+		var highlightedMimeType = START_GREEN + MIME_TYPE + RESET_COLOR;
+
+		var convertedMessage = substringHighlighter.convert(event);
+
+		assertThat(convertedMessage, is(equalTo(LOG_MESSAGE + " with " + highlightedMimeType + " MIME type")));
+	}
+
+	@Test
+	@DisplayName("Log message with multiple MIME types")
+	void processEventWithMultipleMimeTypes() {
+		var event = new LoggingEvent(LOG_MESSAGE_WITH_MIME_TYPE + " and " + MIME_TYPE);
+		var highlightedMimeType = START_GREEN + MIME_TYPE + RESET_COLOR;
+
+		var convertedMessage = substringHighlighter.convert(event);
+
+		assertThat(convertedMessage, is(equalTo(LOG_MESSAGE + " with " + highlightedMimeType + " MIME type and " + MIME_TYPE)));
+	}
+}
