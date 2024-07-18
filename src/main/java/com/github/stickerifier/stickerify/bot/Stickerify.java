7fdca8e35a5d518f715bd21b1da9255b8bc272f2
Roberto Cella <mr.cella.roberto@gmail.com>
2024-07-18 20:23:55 +0200

    Fix expected telegrams error message when the bot cant find the message to reply to (#255)

--- src/main/java/com/github/stickerifier/stickerify/bot/Stickerify.java
+++ src/main/java/com/github/stickerifier/stickerify/bot/Stickerify.java
@@ -151,7 +151,7 @@ public class Stickerify {
 	}
 
 	private void processFailure(TelegramRequest request, TelegramApiException e) {
-		if (e.getMessage().endsWith("Bad Request: message to reply not found")) {
+		if (e.getMessage().endsWith("Bad Request: message to be replied not found")) {
 			LOGGER.atInfo().log("Unable to reply to the {}: the message sent has been deleted", request.getDescription());
 		} else if ("The video could not be processed successfully".equals(e.getMessage())) {
 			LOGGER.atInfo().log("Unable to reply to the {}: the file is corrupted", request.getDescription());
