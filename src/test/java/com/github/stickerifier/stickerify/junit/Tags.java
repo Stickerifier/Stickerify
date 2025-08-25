c1a49ce911356ab0f35316dcacefd416cfbc2adc
Enrico Martelli <mrcnrt95e30@gmail.com>
2025-08-25 19:28:59 +0200

    Fix tags case

--- src/test/java/com/github/stickerifier/stickerify/junit/Tags.java
+++ src/test/java/com/github/stickerifier/stickerify/junit/Tags.java
@@ -2,11 +2,11 @@ package com.github.stickerifier.stickerify.junit;
 
 public interface Tags {
 	String ANIMATED_STICKER = "AnimatedSticker";
-	String CONCURRENT = "concurrent";
+	String CONCURRENT = "Concurrent";
 	String IMAGE = "Image";
 	String LOG = "Log";
 	String MEDIA = "Media";
 	String TELEGRAM_API = "TelegramApi";
-	String UNSUPPORTED_FILE = "unsupportedFile";
-	String VIDEO = "video";
+	String UNSUPPORTED_FILE = "UnsupportedFile";
+	String VIDEO = "Video";
 }
