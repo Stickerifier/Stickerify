6a2d5516be38bb26d3a439d40c80202a82d2c513
Enrico Martelli <mrcnrt95e30@gmail.com>
2025-08-28 13:03:37 +0200

    Reuse constraints constants

--- src/main/java/com/github/stickerifier/stickerify/media/MediaConstraints.java
+++ src/main/java/com/github/stickerifier/stickerify/media/MediaConstraints.java
@@ -10,15 +10,15 @@ package com.github.stickerifier.stickerify.media;
 public final class MediaConstraints {
 
 	static final int MAX_SIDE_LENGTH = 512;
-	static final float MAX_VIDEO_FRAMES = 30F;
-	static final long MAX_VIDEO_DURATION_MILLIS = 3_000L;
+	static final int MAX_VIDEO_FRAMES = 30;
+	static final int MAX_VIDEO_DURATION_MILLIS = 3000;
 	static final String VP9_CODEC = "vp9";
 	static final String MATROSKA_FORMAT = "matroska";
 	static final long MAX_IMAGE_FILE_SIZE = 512_000L;
 	static final long MAX_VIDEO_FILE_SIZE = 256_000L;
 	static final long MAX_ANIMATION_FILE_SIZE = 64_000L;
 	static final int MAX_ANIMATION_FRAME_RATE = 60;
-	static final float MAX_ANIMATION_DURATION_SECONDS = 3F;
+	static final int MAX_ANIMATION_DURATION_SECONDS = 3;
 
 	private MediaConstraints() {
 		throw new UnsupportedOperationException();
