febe084b07713503c87ff6a38c7203b5e2f8e36c
Enrico Martelli <mrcnrt95e30@gmail.com>
2025-08-28 09:50:31 +0200

    Use final class instead

--- src/test/java/com/github/stickerifier/stickerify/junit/Tags.java
+++ src/test/java/com/github/stickerifier/stickerify/junit/Tags.java
@@ -1,12 +1,16 @@
 package com.github.stickerifier.stickerify.junit;
 
-public interface Tags {
-	String ANIMATED_STICKER = "AnimatedSticker";
-	String CONCURRENT = "Concurrent";
-	String IMAGE = "Image";
-	String LOG = "Log";
-	String MEDIA = "Media";
-	String TELEGRAM_API = "TelegramApi";
-	String UNSUPPORTED_FILE = "UnsupportedFile";
-	String VIDEO = "Video";
+public final class Tags {
+	public static final String ANIMATED_STICKER = "AnimatedSticker";
+	public static final String CONCURRENT = "Concurrent";
+	public static final String IMAGE = "Image";
+	public static final String LOG = "Log";
+	public static final String MEDIA = "Media";
+	public static final String TELEGRAM_API = "TelegramApi";
+	public static final String UNSUPPORTED_FILE = "UnsupportedFile";
+	public static final String VIDEO = "Video";
+
+	private Tags() {
+		throw new UnsupportedOperationException();
+	}
 }
