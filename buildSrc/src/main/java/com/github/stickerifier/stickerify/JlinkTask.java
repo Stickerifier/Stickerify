647f8499c1d6bac8dcee54498cc5e33dc0f61ff9
Enrico Martelli <mrcnrt95e30@gmail.com>
2025-10-04 11:43:47 +0200

    Jlink task improvements (#413)

--- buildSrc/src/main/java/com/github/stickerifier/stickerify/JlinkTask.java
+++ buildSrc/src/main/java/com/github/stickerifier/stickerify/JlinkTask.java
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class JlinkTask extends DefaultTask {
@@ -63,49 +62,35 @@ public abstract class JlinkTask extends DefaultTask {
 	public void createJre() {
 		var installationPath = getJavaCompiler().get().getMetadata().getInstallationPath();
 
-		var jlink = installationPath.file("bin/jlink");
-		var jmods = installationPath.dir("jmods");
+		var jlink = installationPath.file("bin/jlink").getAsFile();
+		var jmods = installationPath.dir("jmods").getAsFile();
 
 		var jlinkOutput = getOutputDirectory().dir("jre").get().getAsFile();
 		getFs().delete(deleteSpec -> deleteSpec.delete(jlinkOutput));
 
 		var stdout = new ByteArrayOutputStream();
-		var stderr = new ByteArrayOutputStream();
 
 		var result = getExec().exec(execSpec -> {
 			execSpec.setIgnoreExitValue(true);
 
-			var commandLine = new ArrayList<String>();
-			commandLine.add(jlink.toString());
-			commandLine.addAll(getOptions().get());
-			if (getIncludeModulePath().get()) {
-				commandLine.add("--module-path");
-				commandLine.add(jmods.toString());
-			}
-			commandLine.add("--add-modules");
-			commandLine.add(String.join(",", getModules().get()));
-			commandLine.add("--output");
-			commandLine.add(jlinkOutput.toString());
+			execSpec.setCommandLine(jlink.getAbsolutePath());
+			execSpec.args(getOptions().get());
 
-			execSpec.setCommandLine(commandLine);
+			if (getIncludeModulePath().get()) {
+				execSpec.args("--module-path", jmods.getAbsolutePath());
+			}
+
+			execSpec.args("--add-modules", String.join(",", getModules().get()));
+			execSpec.args("--output", jlinkOutput.getAbsolutePath());
 
 			execSpec.setStandardOutput(stdout);
-			execSpec.setErrorOutput(stderr);
+			execSpec.setErrorOutput(stdout);
 		});
 
 		var stdoutStr = stdout.toString();
-		var stderrStr = stderr.toString();
-
 		if (!stdoutStr.isEmpty()) {
-			getLogger().info(stdoutStr);
-		}
-
-		if (result.getExitValue() != 0) {
-			if (stderrStr.isEmpty()) {
-				getLogger().log(LogLevel.ERROR, "jlink failed with exit code: {}", result.getExitValue());
-			} else {
-				getLogger().log(LogLevel.ERROR, stderrStr);
-			}
+			var level = result.getExitValue() == 0 ? LogLevel.INFO : LogLevel.ERROR;
+			getLogger().log(level, stdoutStr);
 		}
 
 		result.assertNormalExitValue();
